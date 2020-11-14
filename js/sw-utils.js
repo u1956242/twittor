969d0d0fd18f8f93599b19ba8d7840ad622d5297
Iuri <iuri.19.05@gmail.com>
2020-11-14 21:47:15 +0100

    Primer commit

---
+++ js/sw-utils.js
@@ -0,0 +1,11 @@
+// Guardar en el cache dinamico
+function actualizarCacheDinamico(dynamicCache, req, res) {
+    if (res.ok) {
+        caches.open(dynamicCache).then(cache => {
+            cache.put(req, res.clone());
+            return res.clone();
+        });
+    } else {
+        return res;
+    }
+}
